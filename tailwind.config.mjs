fd5290ecb36ea54716f9bf109680ff27ee8c599f
Mario Rodríguez <marioguzman_99@hotmail.com>
2026-01-12 21:50:05 -0600

    Migrando estilos del blog a Tailwind CSS

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        'negro-profundo': '#121212',
+        'blanco-puro': '#FAFAFA',
+        'gris-claro': '#CCCCCC',
+        'gris-medio': '#999',
+        'gris-oscuro': '#333',
+        'verde-lima': '#73ba30'
+      }
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
